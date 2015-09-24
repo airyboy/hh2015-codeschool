4aef3614eaab2ac590ce820628959fa35704d7cd
Mike <Airy>
2015-09-24 13:10:41 +0300

    cleaned up

--- task4.rb
+++ task4.rb
@@ -34,10 +34,6 @@ fib = Enumerator.new do |y|
   end
 end
 
-fib.take(10).each_with_index do |v, i|
-  p "%d - %d - %d" % [i + 1, v, v.exponent]
-end
-
 fib.each_with_index do |n, i|
   if n.exponent >= 1323
     p [i, n, n.exponent]  
